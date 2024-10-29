ad135e0efa19d2716e31e427af502715132bea8f
はっすーず <hassus.linklikelovelive@gmail.com>
2024-10-30 04:18:43 +0900

    ve3

---
+++ js/error.js
@@ -0,0 +1,5 @@
+window.addEventListener('error', function(event) {
+    if (event.target.tagName === 'IMG' || event.target.tagName === 'SCRIPT' || event.target.tagName === 'LINK') {
+        window.location.href = '/404.html';
+    }
+}, true);
