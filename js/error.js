c37aa567cd87b984604bd7031e62bebb50b73737
はっすーず <hassus.linklikelovelive@gmail.com>
2024-10-30 02:34:11 +0900

    ve1

--- js/error.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('error', function(event) {
-    if (event.target.tagName === 'IMG' || event.target.tagName === 'SCRIPT' || event.target.tagName === 'LINK') {
-        window.location.href = '../saiwa-site/404.html';
-    }
-}, true);
